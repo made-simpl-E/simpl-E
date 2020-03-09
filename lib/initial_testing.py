18128ad464be831649407465b2c816ba528a9a90
made-simpl-E <simpl.e.programminglang@gmail.com>
2020-03-08 20:52:31 -0400

    Moved lexing rules back to the SimpleLexer. : - )

--- lib/initial_testing.py
+++ lib/initial_testing.py
@@ -1,10 +1,11 @@
 from lexer import SimpleLexer
 
 
-data = '''one
-one + two
-# hellp
-(two * three)
+data = '''# one
+two = one + 43
+three = (two + 5) * 24 / 6
+list = [0, 1, 2, 3]
+dict = {0: 0, 1: 1, 2: 2}
 '''
 
 if __name__ == '__main__':
--- lib/tokens.py
+++ lib/tokens.py
@@ -15,35 +15,14 @@ tokens = (
     'DIVIDE',
     'LPAREN',
     'RPAREN',
+    'LBRACE',
+    'RBRACE',
+    'LSQBRACE',
+    'RSQBRACE',
+    'PERIOD',
+    'COMMA',
+    'COLON',
     'ID',
     'COMMENT',
 ) + tuple(reserved.values())
-
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'\/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-
-t_ignore = r' '
-t_ignore_COMMENT = r'\#.*'
-
-@TOKEN(r'\d+')
-def t_NUMBER(self, t):
-    t.value = int(t.value)
-    return t
-
-@TOKEN(r'[a-zA-Z_][a-zA-Z_0-9]*')
-def t_ID(self, t):
-    t.type = reserved.get(t.value, 'ID')
-    return t
-
-@TOKEN(r'\n+')
-def t_newline(self, t):
-    t.lexer.lineno += len(t.value)
-
-def t_error(self, t):
-    print(f"Illegal character: {t.value[0]}")
-    t.lexer.skip(1)
 
