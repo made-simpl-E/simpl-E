0e72dd32ef294bab6408d192fa9f96894f78f82d
made-simpl-E <simpl.e.programminglang@gmail.com>
2020-03-11 22:23:54 -0400

    Moved all current tests to the test folder.

---
+++ test/lexer_test.py
@@ -0,0 +1,26 @@
+from os.path import join, abspath, dirname
+from sys import path
+path.insert(0, join(dirname(dirname(abspath(__file__))), 'compiler'))
+
+from simple_lexer import SimpleLexer
+
+
+data = '''# one
+two = one + 43
+three = (two + 5) * 24 / 6
+list = [0, 1, 2, 3]
+dict = {0: 0, 1: 1, 2: 2}
+
+func name(args) {
+    return 4 + args
+}
+
+bool = two == 46.65
+
+name()
+'''
+
+if __name__ == '__main__':
+    lexer = SimpleLexer()
+    lexer.test(data)
+
---
+++ test/parser_test.py
@@ -0,0 +1,14 @@
+from os.path import join, abspath, dirname
+from sys import path
+path.insert(0, join(dirname(dirname(abspath(__file__))), 'compiler'))
+
+from simple_parser import SimpleParser
+from simple_lexer import SimpleLexer
+
+
+if __name__ == '__main__':
+    lexer = SimpleLexer()
+    parser = SimpleParser(lexer)
+
+    parser.test()
+
