da38e778484417ef25b664ca0fa4eae96579c548
made-simpl-E <simpl.e.programminglang@gmail.com>
2020-03-12 19:27:37 -0400

    Fixed test imports.

--- test/parser_test.py
+++ test/parser_test.py
@@ -1,6 +1,6 @@
 from os.path import join, abspath, dirname
 from sys import path
-path.insert(0, join(dirname(dirname(abspath(__file__))), 'compiler'))
+path.insert(0, join(dirname(dirname(abspath(__file__))), 'interpreter'))
 
 from simple_parser import SimpleParser
 from simple_lexer import SimpleLexer
